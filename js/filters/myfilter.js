0e5932ed1e074bb21aaacfd490fa8fcd16ea0f58
wenxiangweng <wwxhmwz@gmail.com>
2014-09-26 10:52:10 +0800

    rm files

--- js/filters/myfilter.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by wengwenxiang on 14-9-10.
- */
-
-angular.module('app').filter('myfilter', function() {
-   return function (item, letter) {
-       return item.toUpperCase();
-   };
-});
--- js/services/sao/myservice.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by wengwenxiang on 14-9-9.
- */
-
-angular.module('app').factory('messageService', function () {
-    var messageService = {};
-    messageService.message = 'hha';
-    return messageService;
-});
